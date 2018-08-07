a86f2587fdd130513dba4489ea0d0518424bae8a
epamatz <40709011+epamatz@users.noreply.github.com>
2018-08-07 15:50:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import jinja2
 import os
 
+
+#libraries for APIs
 from google.appengine.api import urlfetch
 import json
 
@@ -12,22 +14,28 @@ the_jinja_env = jinja2.Environment(
     autoescape=True)
     
     
-
 class mainPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/welcome.html')
         self.response.write(about_template.render())
-    def post(self):
-        self.response.write("recieved a post request")   
+    
 
-class questionsPage(webapp2.RequestHandler):
+
+class infoPage(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(about_template.render())
+    
+class ResultsPage(webapp2.RequestHandler):
     def post(self):
-        self.response.write("recieved answers") 
+        self.response.write("recieved a post request")
+
 
 app = webapp2.WSGIApplication([
     ('/', mainPage),
-    ('/questions', questionsPage),
+    ("/questions", infoPage),
+    ('/results',ResultsPage),
 ], debug=True)
+
+     
+        
